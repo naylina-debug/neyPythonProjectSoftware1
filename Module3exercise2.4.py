ac07d4e7cce2a33886322444e14002f8b2466616
nay <naylina@metropolia.fi>
2026-01-25 22:16:28 +0200

    Exercise 2.4

---
+++ Module3exercise2.4.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+num3 = int(input("Enter the third number: "))
+
+total = num1 + num2 + num3
+product = num1 * num2 * num3
+average = total / 3
+
+print(f"Sum: {total}")
+print(f"Product: {product}")
+print(f"Average: {average}")
